cd6800cc4eb5bab639b8b0024d49adefdde97ebe
Michael Griebling <sam@c-inspirations.com>
2017-06-04 06:23:32 -0400

    Initial Commit

---
+++ Dancing Links/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dancing Links
+//
+//  Created by Mike Griebling on 4 Jun 2017.
+//  Copyright © 2017 Computer Inspirations. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
